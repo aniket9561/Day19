2e9915aed9670943bc8a6bba33f1d10692ea1f27
Aniket <aniketbhatkar12@gmail.com>
2020-07-14 15:03:27 +0530

    Added: Using class methods and variables0

---
+++ EmpWageComProbUC7.java
@@ -0,0 +1,40 @@
+public class EmpWageComProbUC7 {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
+
+	public static int computeEmpWage() {
+
+	int emphrs = 0;
+	int totalemphrs = 0;
+	int totalworkingdays = 0;
+	while (totalemphrs <= MAX_HRS_IN_MONTH &&
+		totalworkingdays < NUM_OF_WORKING_DAYS) {
+	totalworkingdays++;
+	int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+
+	switch (empcheck) {
+		case IS_PART_TIME:
+	 		emphrs = 8;
+			break;
+		case IS_FULL_TIME:
+			emphrs = 12;
+			break;
+		default:
+			emphrs = 0;
+		}
+		totalemphrs += emphrs;
+		System.out.println("Day#: " + totalworkingdays + " Emp Hr: " 
+					+emphrs);
+		}
+	int totalempwage = totalemphrs * EMP_RATE_PER_HOUR;
+	System.out.println("Total Emp wage: " + totalempwage);
+	return totalempwage;
+	}
+
+	public static void main(String[] args){
+		computeEmpWage();
+	}
+}
